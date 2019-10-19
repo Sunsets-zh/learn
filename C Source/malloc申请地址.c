005fe8a1fd6aa0827dff71e69b977ea708122a8f
Sunsets-zh <Sunsets-zh@users.noreply.github.com>
2019-10-20 01:15:52 +0800

    验证归还malloc申请的内存时只能归还首地址

--- C Source/malloc申请地址.c
+++ C Source/malloc申请地址.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+int main(void)
 {
 	void *p;
 	int cnt = 0;
-	while ((p = malloc(100 * 1024 * 1024))) {
-		cnt++;
-	}
-	printf("分配了%d00MB的空间", cnt);
+	p = malloc(100 * 1024 * 1024);//申请100MB 的空间
+	p++;//对p进行操作
+	free(p);//实验对p进行操作后，能否归还p的地址
+	//毫无疑问的失败了
 	return 0;
 }
