cfdd24fc552a4ef03ea08ac2447ff526a9b96ef8
Sunsets-zh <Sunsets-zh@users.noreply.github.com>
2019-11-03 23:27:32 +0800

    struct point

---
+++ C Source/struct point.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+struct point {
+	int x;
+	int y;
+};
+//struct point *m = &mypoint;
+//(*m).x = 12;
+//p->x = 12;//这行程序和上边一行是等同的
+struct point* getStruct(struct point*);//getStruct得到一个结构指针，经过函数运算后返回一个结构指针
+void output(struct point);//这个函数变量是结构体
+void print(const struct point *p);//这个函数变量是结构体指针
+int main()
+{
+	struct point y = { 0,0 };
+	output(y);
+	getStruct(&y);//传的值是指针值（地址）
+	//output(y);
+	output(*getStruct(&y));//这个没有输出？为什么
+	print(getStruct(&y));//这个也没有输出，为什么？
+}
+struct point* getStruct(struct point *p)//传入结构体地址，返回指针
+{
+	scanf("%d", &p->x);
+	scanf("%d", &p->y);
+	printf("%d,%d-get\n", p->x, p->y);
+	return p;
+}
+void output(struct point p)
+{
+	printf("%d,%d-output\n", p.x, p.y);
+}
+void print(const struct point *p)
+{
+	printf("%d,%d-print\n", p->x, p->y);
+
+}
