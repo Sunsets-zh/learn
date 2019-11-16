d5b5fdb387df099cc0d3848d91cb01309a2e39e6
Sunsets-zh <Sunsets-zh@users.noreply.github.com>
2019-11-16 21:37:58 +0800

    将建立链表这一部分抽离出去作为一个函数，这时候可以采用地址的地址的方法来保证函数返回后head是被修改过的

--- C Source/CC/link_list/link_lsit.c
+++ C Source/CC/link_list/link_lsit.c
@@ -8,25 +8,31 @@ int main()
 	do {
 		scanf("%d", &number);
 		if (number != -1) {
-			//加入到link-list里    所以要制造一个link-list，也就是链接起来的Node结构体
-			Node *p = (Node*)malloc(sizeof(Node));//动态分配的
-			/*struct _node r;
-			r.value = number;*/
-			p->value = number;
-			p->next = NULL;//这是新的一个，所以它的末尾是NULL，要让原来最后一个链接过了
-			//找到最后一个
-			Node *last = head;//定义last指向head，进行遍历操作，当last不为NULL时
-			if (last) {//last用next指向下一个，知道他为NULL即最后一个时，跳出
-				while (last->next) {
-					last = last->next;
-				}
-				//wancheng
-				last->next = p;//把最后一个链接到新建的那个上 
-			}
-			else {
-				head = p;//如果一开始last就是NULL，它指向的那个head就是NULL，那么head=p；
-			}
+			add(&head, number);
 		}
 	} while (number != -1);
 	return 0;
+}
+void add(Node** phead,int number)
+{
+	//加入到link-list里    所以要制造一个link-list，也就是链接起来的Node结构体
+	Node *p = (Node*)malloc(sizeof(Node));//动态分配的
+	/*struct _node r;
+	r.value = number;*/
+	p->value = number;
+	p->next = NULL;//这是新的一个，所以它的末尾是NULL，要让原来最后一个链接过了
+	//找到最后一个
+	Node *last = phead;//定义last指向head，进行遍历操作，当last不为NULL时
+	if (last) {//last用next指向下一个，知道他为NULL即最后一个时，跳出
+		while (last->next) {
+			last = last->next;
+		}
+		//wancheng
+		last->next = p;//把最后一个链接到新建的那个上 
+	}
+	else {
+		phead = p;//如果一开始last就是NULL，它指向的那个head就是NULL，那么head=p；
+		//这样存在一个问题，即操作后的head和主函数没什么关系，这就很尴尬，所以给地址套上地址
+		//这样直接修改地址的地址里的东西，head就被修改了，可以直接返回
+	}
 }
